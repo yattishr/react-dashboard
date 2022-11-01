658a80903ba701cc8a83857d61eb9d0be1888a69
yattishr <yattish@gmail.com>
2022-11-01 14:19:36 +0200

    completed adding Contact form.

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -1,13 +1,157 @@
-import { Box } from "@mui/material";
+import { Box, IconButton, Typography, TextField, Button, useTheme } from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { ColorModeContext, tokens } from "../../theme";
+
 import { Header } from "../../components/Header";
 
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  address1: "",
+  address2: "",
+}
+
+const phoneRegExp =  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const userSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("Email is inavlid.").required("required"),
+  contact: yup.string().matches(phoneRegExp, "Phone number is invalid.").required("required"),
+  address1: yup.string().required("required"),
+  address2: yup.string().required("required"),
+});
+
 const Form = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const handleFormSubmit = (values)   => {
+    console.log(`Logging form input: ${JSON.stringify(values)}`);
+  }
+
   return (
     <Box m="20px">
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="FORM" subtitle="Reportstastic ~ Forms Page" />
+        <Header title="CREATE USER" subtitle="Create User Profile" />
+        <Formik
+          onSubmit={handleFormSubmit}
+          initialValues={initialValues}
+          validationSchema={userSchema}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              {/* form container */}
+              <Box
+                display="grid"
+                gap="30px"
+                gridTemplateColumns="repeat(4, minmax(0, 1fr)"
+                sx={{
+                  "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+                }}
+              >
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="First Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.firstName}
+                  name="firstName"
+                  error={ !!touched.firstName && !!errors.firstName }
+                  helperText={ touched.firstName && errors.firstName }
+                  sx={{ gridColumn: "span 2" }}                  
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Last Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.lastName}
+                  name="lastName"
+                  error={ !!touched.lastName && !!errors.lastName }
+                  helperText={ touched.lastName && errors.lastName }
+                  sx={{ gridColumn: "span 2" }}                  
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.email}
+                  name="email"
+                  error={ !!touched.email && !!errors.email }
+                  helperText={ touched.email && errors.email }
+                  sx={{ gridColumn: "span 4" }}                  
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Contact number"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.contact}
+                  name="contact"
+                  error={ !!touched.contact && !!errors.contact }
+                  helperText={ touched.contact && errors.contact }
+                  sx={{ gridColumn: "span 4" }}                  
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Address 1"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.address1}
+                  name="address1"
+                  error={ !!touched.address1 && !!errors.address1 }
+                  helperText={ touched.address1 && errors.address1 }
+                  sx={{ gridColumn: "span 2" }}                  
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Address 2"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.address2}
+                  name="address2"
+                  error={ !!touched.address2 && !!errors.address2 }
+                  helperText={ touched.address2 && errors.address2 }
+                  sx={{ gridColumn: "span 2" }}                  
+                />                                                                         
+              </Box>
+              
+              {/* button container */}
+              <Box display="flex" justifyContent="end" mt="20px">
+                <Button type="submit" color={colors.primary[400]} variant="container">
+                  Create User
+                </Button>
+              </Box>
+
+            </form>
+          )}
+        </Formik>
       </Box>
-    </Box>
   );
 };
 
---
+++ src/scenes/global/Template.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import { Header } from "../../components/Header";
+
+const FAQ = () => {
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="Template" subtitle="Reportstastic ~ Template Page" />
+      </Box>
+    </Box>
+  );
+};
+
+export default FAQ;
